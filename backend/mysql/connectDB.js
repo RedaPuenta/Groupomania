540dee9757cb6d70e1dd097caa6a9df38d9cb29d
Reda Puenta <rida.hadj@hotmail.com>
2025-01-02 15:34:31 +0100

    fix: host env

--- backend/mysql/connectDB.js
+++ backend/mysql/connectDB.js
@@ -1,21 +1,21 @@
-require("dotenv").config()
-const mysql = require('mysql2')
+require("dotenv").config();
+const mysql = require("mysql2");
 
 //! Fonction qui permet de s'authentifier à la base de donnée
 const db = mysql.createConnection({
-    host: 'localhost',
-    user: process.env.MYSQL_USER,
-    password: process.env.MYSQL_PASSWORD,
-    database: process.env.MYSQL_DATABASE
-})
+  host: process.env.MYSQL_HOST,
+  user: process.env.MYSQL_USER,
+  password: process.env.MYSQL_PASSWORD,
+  database: process.env.MYSQL_DATABASE,
+});
 
 //! Fonction qui permet de se connecter à la base de donnée
-db.connect((error)=> {
-    if(error){
-        console.log("Connexion échouée à MySQL --> " + error)
-    } else {
-        console.log("Connexion réussi à MySQL")
-    }
-})
+db.connect((error) => {
+  if (error) {
+    console.log("Connexion échouée à MySQL --> " + error);
+  } else {
+    console.log("Connexion réussi à MySQL");
+  }
+});
 
 module.exports = db;
